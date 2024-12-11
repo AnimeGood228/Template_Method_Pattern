8d8d051c2f2ca815641241755a356963d7503156
1 <1@1MISA>
2024-12-12 01:17:15 +0700

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Template_Method_Pattern")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Template_Method_Pattern")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("857f0b14-4e3e-4bf2-b01a-530d194edc88")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReportGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public abstract class ReportGenerator
+{
+    // Шаблонный метод
+    public void GenerateReport()
+    {
+        CollectData();
+        ProcessData();
+        FormatReport();
+        ExportReport();
+    }
+    protected abstract void CollectData();
+    protected abstract void ProcessData();
+    protected abstract void FormatReport();
+
+    // Конкретный метод
+    private void ExportReport()
+    {
+        Console.WriteLine("Отчет сделан.");
+    }
+}
+public class StudentReportGenerator : ReportGenerator
+{
+    protected override void CollectData()
+    {
+        Console.WriteLine("Собраны данные о студентах.");
+    }
+    protected override void ProcessData()
+    {
+        Console.WriteLine("Обработаны данные о студентах.");
+    }
+    protected override void FormatReport()
+    {
+        Console.WriteLine("Отчет о студентах отформатирован.");
+    }
+}
+public class CourseReportGenerator : ReportGenerator
+{
+    protected override void CollectData()
+    {
+        Console.WriteLine("Собраны данные о курсах.");
+    }
+    protected override void ProcessData()
+    {
+        Console.WriteLine("Обработаны данные о курсах.");
+    }
+    protected override void FormatReport()
+    {
+        Console.WriteLine("Отчет о курсах отформатирован.");
+    }
+}
